0c146e211a9bfc7bdc86582a992539f0842b8700
kilian <kilian.leroux@epsi.fr>
2019-11-12 14:38:59 +0100

    Ajoutez des fichiers projet.

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using RendueCode.ORM;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RendueCode.Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (var db = new ContexteBDD())
+            {
+          
+            }
+        }
+    }
+}
